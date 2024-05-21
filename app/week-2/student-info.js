19fd9b275b66fedcf417aee839e256503c070832
kwin256 <kwin256wei@gmail.com>
2024-05-21 13:23:50 -0600

    week-2
    commit assignments

---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+export default function StudentInfo() {
+        return (
+            <main>
+                <p>Name: Xuekun Wei</p>
+                <p>GitHub repository: https://github.com/kwin256/cprg306-assignments</p>
+            </main>
+        );
+    }
