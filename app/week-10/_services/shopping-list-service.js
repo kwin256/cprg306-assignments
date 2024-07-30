344648c062536afe7ee2d59d79825dfe3cc0f2cf
kwin256 <kwin256wei@gmail.com>
2024-07-30 12:02:34 -0600

    week10 update

--- app/week-10/_services/shopping-list-service.js
+++ app/week-10/_services/shopping-list-service.js
@@ -5,15 +5,17 @@ import { collection, getDocs, addDoc, query } from "firebase/firestore";
 const getItems = async (userId) => {
     const q = query(collection(db, "users", userId, "items"));
     const querySnapshot = await getDocs(q);
-    if (querySnapshot.empty) {
-        return [];
-    }
-    return querySnapshot.map( doc => ({id: doc.id, ...doc.data()}) );
+
+    let items = [];
+    querySnapshot.forEach((doc) => {
+        items.push({...doc.data()});
+      });
+    return items;
 }
 
 const addItem = async (userId, item) => {
     const docRef = await addDoc(collection(db, "users", userId, "items"), item);
     return docRef.id;
-}
+}   
 
 export { getItems, addItem };
